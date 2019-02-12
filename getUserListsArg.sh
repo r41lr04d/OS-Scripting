ad9015b4975e4ec1694b9bd27bc14635ef5bcfd9
rail road <r41lr04d@gmail.com>
2019-02-12 20:01:50 +0700

    Update getUserListsArg.sh

--- getUserListsArg.sh
+++ getUserListsArg.sh
@@ -4,4 +4,22 @@ HOSTR="$1"
 USERIDR="$2"
 PASSWDR="$3"
 PATHDIR="/root/GetListUsers/"
-sshpass -p $PASSWDR ssh -o StrictHostKeyChecking=no $USERIDR@$HOSTR 'getent passwd' > $PATHDIR$HOSTR".txt" && cat $PATHDIR$HOSTR".txt" | awk -F":" '{ print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "," $7 "," "'"$HOSTR"'" }' > $PATHDIR$HOSTR".csv"
+UNAMEA=""
+NAMAHOST=""
+##GET UNAME
+UNAMEA=$(sshpass -p $PASSWDR ssh -o StrictHostKeyChecking=no $USERIDR@$HOSTR 'uname -a');
+
+##GET HOSTNAME
+NAMAHOST=$(sshpass -p $PASSWDR ssh -o StrictHostKeyChecking=no $USERIDR@$HOSTR 'hostname');
+
+##GET ETC PASSWD
+sshpass -p $PASSWDR ssh -o StrictHostKeyChecking=no $USERIDR@$HOSTR 'getent passwd' > $PATHDIR$HOSTR"-passwd.txt" && cat $PATHDIR$HOSTR"-passwd.txt" | awk -F":" '{ print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "," $7 "," "'"$HOSTR"'" "," "'"$NAMAHOST"'" "," "'"$UNAMEA"'" }' > $PATHDIR$HOSTR"-passwd.csv";
+printf "Export ETC-PASSWD Successfull\n";
+sleep 1s;
+###GET ETC GROUP
+sshpass -p $PASSWDR ssh -o StrictHostKeyChecking=no $USERIDR@$HOSTR 'getent group' > $PATHDIR$HOSTR"-group.txt"  && cat $PATHDIR$HOSTR"-group.txt" | awk -F":" '{ print $1 "," $2 "," $3 "," $4 "," "'"$HOSTR"'" }' > $PATHDIR$HOSTR"-group.csv";
+printf "Export ETC-GROUP Successfull\n";
+
+###GET SUDOERS
+sshpass -p $PASSWDR ssh -o StrictHostKeyChecking=no $USERIDR@$HOSTR 'cat /etc/sudoers' > $PATHDIR$HOSTR"-sudoers.txt" && cat $PATHDIR$HOSTR"-sudoers.txt" | egrep -v "^\s*(#|$)" > $PATHDIR$HOSTR"-sudoers.csv";
+printf "Export ETC-SUDOERS Successfull\n";
